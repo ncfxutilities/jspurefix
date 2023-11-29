aefd6d4aee975372a54940130df36360b28f3b56
Pak Hei Li <issacjohannli@gmail.com>
2023-11-29 13:39:06 +0000

    Export * from './FIX4.4/repo';

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,5 @@
-export * from './enum'
+export * from './FIX4.4/repo';
+export * from './enum/';
+export { MsgTag } from './enum/msg_tag';
+export { MsgType } from './enum/msg_type';
+export { SessionRejectReason } from './enum/sess_rej_rsn';
